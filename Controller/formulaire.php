32971daf9488b9b823589ca5bb9bf6ca3fdaff0e
Administrateur <administrateur@MacBook-Air-de-Administrateur.local>
2020-10-15 11:36:29 +0200

    correction bug

--- Controller/formulaire.php
+++ Controller/formulaire.php
@@ -2,7 +2,6 @@
    include_once "../View/contact.php"; 
    include_once "../Model/bdd.php";
    $bdd = new Bdd();
-   $Expedideur = $bdd->insertExpediteur();
 
 if (isset($_POST["nom"]) && isset($_POST["prenom"]) && isset($_POST["email"])  && isset($_POST["ville"]) && isset($_POST["message"])) {
 
